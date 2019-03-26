8c9e71d4861f9432b9269f1bd98a4e66bdb7ed3f
Yauheni Yarotski <yauheni_yarotski@me.com>
2019-03-26 11:30:43 +0300

    back object mapper

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
-        .package(url: "https://github.com/Hearst-DD/ObjectMapper.git", from: "3.2.0"),
+        .package(url: "https://github.com/Hearst-DD/ObjectMapper.git", from: "3.4.2"),
         .package(url: "https://github.com/vapor/websocket.git", from: "1.0.0")
     ],
     targets: [
