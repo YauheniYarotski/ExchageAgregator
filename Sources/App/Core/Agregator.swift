9f678e34d1a61b7786ca0ea8bc15b29efa17c34d
Yauheni Yarotski <yauheni_yarotski@me.com>
2019-04-10 00:08:32 +0300

    added rest calls

--- Sources/App/Core/Agregator.swift
+++ Sources/App/Core/Agregator.swift
@@ -26,24 +26,27 @@ class Agregator {
       for pair in exchange.value {
         var asks = [Double:Double]()
         var bids = [Double:Double]()
-        var totalAsks: Double = 0
-        var totalBids: Double = 0
         for levelPrice in pair.value {
           let price = levelPrice.key.granulate(toGranulation: granulation)
           let amount = levelPrice.value
           if price < 0 {
             asks[-price] =  (asks[-price] ?? 0) + amount
-            totalAsks = totalAsks + amount
           } else {
             bids[price] =  (bids[price] ?? 0) + amount
-            totalBids = totalBids + amount
             
             
           }
         }
         
-        let sorteredAsks = asks.sorted(by: {$0.0 < $1.0}).map({[$0.key.rounded(toPlaces: 2),$0.value.rounded(toPlaces: 2)]})
-        let sorteredBids = bids.sorted(by: {$0.0 > $1.0}).map({[$0.key.rounded(toPlaces: 2),$0.value.rounded(toPlaces: 2)]})
+        let sorteredAsks = asks.sorted(by: {$0.0 < $1.0}).prefix(30).map({[$0.key.rounded(toPlaces: 2),$0.value.rounded(toPlaces: 2)]})
+        let sorteredBids = bids.sorted(by: {$0.0 > $1.0}).prefix(30).map({[$0.key.rounded(toPlaces: 2),$0.value.rounded(toPlaces: 2)]})
+        
+        let totalAsks = sorteredAsks.reduce(0.0) { result, nextAsk in
+          return result + nextAsk[1]
+        }
+        let totalBids: Double = sorteredBids.reduce(0.0) { result, nextBid in
+          return result + nextBid[1]
+        }
         
         let bookForPair = BookForPair.init(pair: pair.key, asks: sorteredAsks, bids: sorteredBids, totalAsks: totalAsks.rounded(toPlaces:3), totalBids: totalBids.rounded(toPlaces:3))
         booksForPairs.append(bookForPair)
